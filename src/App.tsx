263ba0306ab79b48e684c23cb4b0dda0f01d9b75
Bilal Ahmad <bilal.ahmad@codedistrict.com>
2023-07-28 12:09:21 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {lazy, Suspense} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/home'));
+const About = lazy(() => import('./pages/about'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="center">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+              {/* Add more routes for other pages */}
+            </Routes>
+          </Suspense>
+          <button onClick={() => alert('Button 1')}>Button 1</button>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+// pages/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>About page body content</p>
+    </div>
+  );
+}
+
+export default About;

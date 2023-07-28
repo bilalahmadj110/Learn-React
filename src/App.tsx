742c8cad558e0eef40887ef7bace240675551bd0
Bilal Ahmad <bilal.ahmad@codedistrict.com>
2023-07-28 12:46:32 +0500

    simple box

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React, {lazy, Suspense} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import Box from "./components/box";
 
-const Home = lazy(() => import('./pages/home'));
-const About = lazy(() => import('./pages/about'));
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <div className="center">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/about" element={<About/>}/>
-              {/* Add more routes for other pages */}
-            </Routes>
-          </Suspense>
-          <button onClick={() => alert('Button 1')}>Button 1</button>
-        </div>
-      </div>
-    </Router>
-  );
+    return (
+        <>
+            <Box/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/box.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./box.css";
+
+const Box = () => {
+    return (
+        <div className="box">
+            <h1>Box</h1>
+        </div>
+    );
+}
+
+export default Box;
